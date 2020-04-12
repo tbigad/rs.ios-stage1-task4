9945b1c8755034b4deedd0b7a604fff54d0d040e
Pavel Nadolski <tbigad@gmail.com>
2020-04-13 02:56:22 +0300

    Update SquareDecomposer.m

--- RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
+++ RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
@@ -2,7 +2,30 @@
 
 @implementation SquareDecomposer
 - (NSArray <NSNumber*>*)decomposeNumber:(NSNumber*)number {
-    // good luck
-    return @[@0];
+    int initialNumber = [(NSNumber*) number intValue];
+    return [self decomposeNumberRec:initialNumber leftSpace:pow(initialNumber, 2)];
 }
+
+- (NSMutableArray <NSNumber*>*)decomposeNumberRec:(int)initialNumber
+                                 leftSpace:(long) leftSpace {
+    int maximalClosestNumber = initialNumber - 1;
+    NSMutableArray *res = [[NSMutableArray alloc] init];
+    if (leftSpace == 0) {
+        return res;
+    }
+    while (maximalClosestNumber > 0) {
+        int leftSpaceAfterSubtraction = leftSpace - pow(maximalClosestNumber, 2);
+        if (leftSpaceAfterSubtraction >= 0) {
+            res = [self decomposeNumberRec:maximalClosestNumber
+                                 leftSpace:leftSpaceAfterSubtraction];
+            if (res != nil) {
+                [res addObject:@(maximalClosestNumber)];
+                return res;
+            }
+        }
+        maximalClosestNumber--;
+    }
+    return nil;
+}
+
 @end
